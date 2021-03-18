ddb622d73d9dc7996bf810f2cf5bc70e34eecc65
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-18 13:57:04 -0300

    Create shift_left_16

---
+++ parts_made/shift_left_16.v
@@ -0,0 +1,11 @@
+module shift_left_16(
+    input   wire    [15:0]  Immediate,
+    output  wire    [31:0]  Shif_left_16_out
+);
+
+    wire [31:0] aux;
+
+    assign Aux = {{16{1'b0}}, Immediate}; //Extends Immediate from 16bits to 32bits
+    assign Shif_left_16_out = Aux << 16;
+
+endmodule
