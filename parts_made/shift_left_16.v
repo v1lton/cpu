a2fbd6af971541c687a5fde6e7fdbf59dd7a8026
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-19 08:48:19 -0300

    Fixes Aux

--- parts_made/shift_left_16.v
+++ parts_made/shift_left_16.v
@@ -3,7 +3,7 @@ module shift_left_16(
     output  wire    [31:0]  Shif_left_16_out
 );
 
-    wire [31:0] aux;
+    wire [31:0] Aux;
 
     assign Aux = {{16{1'b0}}, Immediate}; //Extends Immediate from 16bits to 32bits
     assign Shif_left_16_out = Aux << 16;
